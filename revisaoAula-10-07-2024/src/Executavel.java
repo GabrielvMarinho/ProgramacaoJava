2de9994c69f7f5e81d48489a254accaa7013a6e6
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-07-16 16:59:23 -0300

    AS
    DASD

--- revisaoAula-10-07-2024/src/Executavel.java
+++ revisaoAula-10-07-2024/src/Executavel.java
@@ -2,13 +2,17 @@ public class Executavel {
     public static void main(String arg[]){
 
         ItemBiblioteca livro = new Livro("atividades de banco de dados para pessoas inteligentes", 0, 0, "0" ,"0" );
+          Biblioteca biblioteca = new Biblioteca();
 
-      System.out.println(Biblioteca.buscarItemPorTitulo("atividades de banco de dados para pessoas inteligentes"));
-      System.out.println("---------------");
-      ItemBiblioteca revista = new Revista("Revista Veja", 0, 0, 0 );
-      Biblioteca.listarItens();
-      Biblioteca.adicionarItem(livro);
-      Biblioteca.atualizarItem(0, revista);
-      Biblioteca.listarItens();
+        System.out.println(biblioteca.buscarItemPorTitulo("atividades de banco de dados para pessoas inteligentes"));
+        System.out.println("---------------");
+        ItemBiblioteca revista = new Revista("Revista Veja", 0, 0, 0 );
+        Biblioteca.listarItens();
+        Biblioteca.adicionarItem(livro);
+        Biblioteca.atualizarItem(0, revista);
+        Biblioteca.listarItens();
+
+
+        Biblioteca.atualizarItem(0, revista);
     }
 }
