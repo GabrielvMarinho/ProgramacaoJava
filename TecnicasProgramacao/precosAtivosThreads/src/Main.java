7af87cccbb0ea53d57bcbb39ba64474c640b053f
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-10-11 20:20:19 -0300

    nada

--- TecnicasProgramacao/precosAtivosThreads/src/Main.java
+++ TecnicasProgramacao/precosAtivosThreads/src/Main.java
@@ -4,10 +4,11 @@ public class Main extends Thread{
     public static void main(String[] args) {
 
         Thread thread1 = new Moeda("--BITCOIN--", "BTCUSDT", 1000);
-        Thread thread2 = new Moeda("--ETHEREUM--", "ETHUSDT", 1000);
+        Thread thread2 = new Moeda("--ETHEREUM--", "ETHUSDT", 3000);
 
         thread1.start();
         thread2.start();
+
 
 
     }
